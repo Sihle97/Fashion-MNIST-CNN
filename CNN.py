0615dd8c22e84104f736467dc520a266c77d66fd
sihle <sihlesakhile97@gmail.com>
2022-07-06 23:09:19 +0200

    First evaluation with test data then performing some data augmentation

--- CNN.py
+++ CNN.py
@@ -127,3 +127,26 @@ batch_sz = 128
 num_epochs = 100
 
 model.fit(X_train, y_train, batch_size=batch_sz, epochs=num_epochs, validation_data=(X_val, y_val), verbose=1)
+
+score = model.evaluate(X_test, y_test) 
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+# With data augmentation to prevent overfitting
+
+gen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
+
+gen2 = ImageDataGenerator()
+
+train_gen = gen.flow(X_train, y_train, batch_size=batch_sz)
